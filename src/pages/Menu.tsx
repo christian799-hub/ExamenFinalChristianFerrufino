82f3b2ba017c9d2cbd14d8f0b54f495a373da529
christian799-hub <christian.ferrufino.m@ucb.edu.bo>
2026-06-12 01:14:00 -0400

    Menu3

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,11 +1,14 @@
-import logoFerreteria from '../assets/Logo2.png'; 
+import logoFerreteria from '../assets/Logo2.png';
 import './Menu.css';
 
 const Menu = () => {
   return (
-    <div className='card'>
-      <h1>Sistema de facturacion Ferreteria America</h1>
-      {logoFerreteria}
+    <div className="menu-container">
+      <div className="card">
+        <img src={logoFerreteria} alt="Logo Ferretería América" className="logo" />
+        <h1>Sistema de facturación<br />Ferretería América</h1>
+        <p>Bienvenido al panel de control</p>
+      </div>
     </div>
   );
 };
